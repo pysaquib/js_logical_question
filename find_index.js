dc01874f7ff1842a2b8241652f87d0adbf3c9e63
sarmistha <64093543+sarmisthamaity@users.noreply.github.com>
2021-01-18 17:19:22 +0530

    Add files via upload

---
+++ find_index.js
@@ -0,0 +1,35 @@
+// var twoSum = function(nums, target) {
+//     index=0
+//     array=[]
+//     while(index<nums.length){
+//         j=0
+//         while(j<nums.length){
+//             if (nums[index]+nums[j]==target){
+//                 array.push(index)
+//             }
+//             j++
+//         }
+//         index++
+//     }
+//     console.log(array)
+// };
+// twoSum([3,2,4],6)
+
+// var twoSum=function(nums,target){
+//     number=[]
+//     for(let index=0;index<nums.length;index++){
+//         for(let j=0;j<nums.length;j++){
+//             if(nums[index]+nums[j]==target){
+//                 if(!(index==j)){
+//                     number.push(index)
+//                 }
+              
+//             }
+//         }
+//     }
+//     return(number)
+// }
+// twoSum([3,2,4],6)
+
+
+
